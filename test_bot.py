59d34ae4730fafd453691fa0c3ad143f0e584483
alkanneyup <eyupalkan02@gmail.com>
2026-08-16 23:24:10 +0300

    Update test_bot.py

--- test_bot.py
+++ test_bot.py
@@ -2,8 +2,17 @@ import requests
 
 URL = "https://kariyerkapisi.gov.tr/"
 
-response = requests.get(URL, timeout=30)
+response = requests.get(
+    URL,
+    headers={
+        "User-Agent": "Mozilla/5.0"
+    },
+    timeout=30
+)
 
-print("HTTP DURUMU:", response.status_code)
-print("SAYFA UZUNLUĞU:", len(response.text))
-print("BAĞLANTI BAŞARILI")
+print("HTTP:", response.status_code)
+print("UZUNLUK:", len(response.text))
+
+print("\n--- SAYFA BAŞLANGICI ---")
+print(response.text[:5000])
+print("\n--- SAYFA SONU ---")
